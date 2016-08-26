0daea0725b4b0631a4c230411af987bbf48afdf3
bchantech <b_chan1@u.pacific.edu>
2016-08-25 21:18:53 -0700

    Added Watchlist class

---
+++ RobinhoodNet/Watchlist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicallyMe.RobinhoodNet
+{
+    class Watchlist
+    {
+        Url<Instrument> Instrument { get; set; }
+        DateTime CreatedAt { get; set; }
+        Url<Watchlist> Url;
+
+        public Watchlist() { }
+
+        internal Watchlist(Newtonsoft.Json.Linq.JToken json) : this()
+        {
+            Instrument = new Url<Instrument>((string)json["instrument"]);
+            CreatedAt = (DateTime)json["created_at"];
+            Url = new Url<Watchlist> ((string)json["url"]);
+        }
+    }
+}
